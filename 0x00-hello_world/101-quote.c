f3618ab15d610703127bb54227f895801fe9c2d5
Ahmed190799 <ahmedmedu190799@gmail.com>
2023-03-17 12:14:30 -0700

    standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - prints to stanard error
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	return (1);
+}
