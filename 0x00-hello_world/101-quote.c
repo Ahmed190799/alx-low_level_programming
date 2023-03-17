f32303326eeee34f24f98240b424b808135465e6
Ahmed190799 <ahmedmedu190799@gmail.com>
2023-03-17 14:01:44 -0700

    stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
-#include <stdio.h>
+
 /**
  * main - prints to stanard error
  * Return: 1 (success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful.\" - Dora Korpar, 2015-10-19", stderr);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
