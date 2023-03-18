be9a0c46c59925bd27fae3996674261fb680fd48
Ahmed190799 <ahmedmedu190799@gmail.com>
2023-03-18 13:05:22 -0700

    low case alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - alphabets
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	putchar(lc);
+	putchar('\n');
+	return (0);
+}
+
