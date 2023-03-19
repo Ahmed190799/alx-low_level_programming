6c7f495765e6660b4ecc7914c4a7027978cf377d
Ahmed190799 <ahmedmedu190799@gmail.com>
2023-03-19 10:00:01 -0700

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,10 +9,12 @@ int main(void)
 	int i;
 	char c;
 
-	for (i = 0; i <= 9; i++)
-	for (c = 'a'; c <= 'f'; c++)
+	for (i = '0'; i <= '9'; i++)
 	putchar(i);
+
+	for (c = 'a'; c <= 'f'; c++)
 	putchar(c);
 	putchar('\n');
+
 	return (0);
 }
