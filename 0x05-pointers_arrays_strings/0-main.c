f8c2b6dbd71df1d8d72072444dd60933fd3526fb
Ahmed190799 <ahmedmedu190799@gmail.com>
2023-03-27 19:38:50 -0700

    strings

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -1,7 +1,8 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - check the code
+ * main - check the code 
  *
  * Return: Always 0.
  */
@@ -9,9 +10,9 @@ int main(void)
 {
 	int n;
 
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
+    n = 402;
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+    return (0);
 }
