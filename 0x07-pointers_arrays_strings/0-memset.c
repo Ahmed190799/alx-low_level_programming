59cba9571cff3c1b6d87a6f3cae1daf2a322c1e0
Ahmed190799 <ahmedmedu190799@gmail.com>
2023-04-03 16:18:41 -0700

    header files

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memset - Entry point
+ * @n: bytes
+ * @s: pointer destination
+ * @b: constant byte
+ * Return - s
+ */
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int i;
+
+	if (i = 0, i < n, i++)
+		s[i] = b;
+	return (s);
+}
+
